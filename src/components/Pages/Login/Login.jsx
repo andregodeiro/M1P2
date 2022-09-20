e6960b17e9e5bc2bf31f2d4a4c04599156db57a4
André Godeiro <andrevictor.godeiro@gmail.com>
2022-09-19 23:48:34 -0300

    feat: useForm and CEP API

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,21 +1,42 @@
 import { Button, ContainerLogin } from "../../../styles";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
 export const Login = () => {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const LoginFunc = (details) => {
+    console.log(details);
+  };
+
+  const Logout = () => {
+    console.log("Logout");
+  };
+
   return (
     <ContainerLogin>
       <div>
         <img src="https://i.imgur.com/pTj5VAl.png" alt="" />
-        <form action="" className="formLoginContainer">
+        <form className="formLoginContainer">
           <div className="labelInput">
-            <label htmlFor="">e-mail</label>
+            <label htmlFor="email" name="email" id="email">
+              e-mail
+            </label>
             <input type="text" />
-            <label htmlFor="">senha</label>
-            <input type="password" />
+            <label htmlFor="password">senha</label>
+            <input type="password" name="password" id="password" />
           </div>
           <div className="buttons">
             <Button>
-              <Link to="/home">acessar</Link>
+              <Link to="/home">
+                <input type="submit" value="acessar" />
+              </Link>
             </Button>
             <Link to="/cadastro">crie uma conta no connectlab</Link>
           </div>
--- src/components/Pages/SignUp/SignUp.jsx
+++ src/components/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,27 @@
 import { Button, FormSignUp, FormSignUpHeader } from "../../../styles";
+import { useForm } from "react-hook-form";
 
 export const SignUp = () => {
+  const { register, handleSubmit, setValue, setFocus } = useForm();
+
+  const onSubmit = (e) => {
+    console.log(e);
+  };
+
+  const catchCep = (e) => {
+    const cep = e.target.value.replace(/\D/g, "");
+    console.log(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`).then((res) =>
+      res.json().then((data) => {
+        setValue("adress", data.logradouro);
+        setValue("district", data.bairro);
+        setValue("city", data.localidade);
+        setValue("state", data.uf);
+        setFocus("adressNumber");
+      })
+    );
+  };
+
   return (
     <div>
       <FormSignUpHeader>
@@ -12,55 +33,64 @@ export const SignUp = () => {
       </FormSignUpHeader>
       <div>
         <FormSignUp>
-          <form action="" className="formSignUp">
+          <form onSubmit={handleSubmit(onSubmit)} className="formSignUp">
             <div className="signUpData1">
               <label htmlFor="">Nome Completo</label>
-              <input type="text" />
+              <input type="text" {...register("name")} />
 
               <label htmlFor="">E-mail</label>
-              <input type="text" />
+              <input type="text" {...register("email")} />
 
               <label htmlFor="">Senha</label>
-              <input type="password" />
+              <input type="password" {...register("password")} />
             </div>
             <div className="signUpData2">
               <label htmlFor="">Foto de Perfil</label>
-              <input type="text" placeholder="Insira uma URL" />
+              <input
+                type="text"
+                placeholder="Insira uma URL"
+                {...register("avatar")}
+              />
 
               <label htmlFor="">Telefone</label>
-              <input type="number" />
+              <input type="number" {...register("telephone")} />
 
               <label htmlFor="">Confirme a senha</label>
-              <input type="password" />
+              <input type="password" {...register("passwordConfirm")} />
             </div>
 
             <div className="signUpAdress1">
               <label htmlFor="">CEP</label>
-              <input type="text" />
+              <input
+                type="text"
+                id="cep"
+                {...register("cep")}
+                onBlur={catchCep}
+              />
 
               <label htmlFor="">Logradouro</label>
-              <input type="text" />
+              <input type="text" id="logradouro" {...register("adress")} />
 
               <label htmlFor="">Número</label>
-              <input type="number" />
+              <input type="number" id="numero" {...register("adressNumber")} />
             </div>
             <div className="signUpAdress2">
               <label htmlFor="">Bairro</label>
-              <input type="text" />
+              <input type="text" id="bairro" {...register("district")} />
 
               <label htmlFor="">Cidade</label>
-              <input type="text" />
+              <input type="text" id="cidade" {...register("city")} />
 
               <label htmlFor="">Estado</label>
-              <input type="text" />
+              <input type="text" id="uf" {...register("state")} />
 
               <label htmlFor="">Complemento</label>
-              <input type="text" />
+              <input type="text" id="complemento" {...register("comp")} />
+            </div>
+            <div className="sendButton">
+              <Button type="submit"> cadastrar</Button>
             </div>
           </form>
-          <div className="sendButton">
-            <Button>Cadastrar</Button>
-          </div>
         </FormSignUp>
       </div>
     </div>
